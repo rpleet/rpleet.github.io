568ce8f0b90489a013aae6d9e6372fc77a0ab63a
rpleet <leet@hirsiz.ru>
2023-03-18 17:42:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode, volume, language ) {
-    let steamid = steamid
-}
-
 $(document).ready(function () {
     $.ajax({
-        url: "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=5534AF9696A18889B116F974530A1065&steamids=" + steamid,
+        url: "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=5534AF9696A18889B116F974530A1065&steamids=76561198824775250",
     }).done(function (data) {
         var json = JSON.parse(data);
         $('#playerName').text(json['response']['players'][0]['personaname']);
